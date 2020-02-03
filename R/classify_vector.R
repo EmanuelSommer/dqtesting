5cee718653dd72157b5239de208de45cbf1c885f
Emanuel Sommer <emanuel.sommer@web.de>
2020-02-03 09:27:11 +0100

    + classify_vector function + unit tests classify_vector function

---
+++ R/classify_vector.R
@@ -0,0 +1,35 @@
+#' Determine whether the vector will be classified as: continuous, categorical, datetime or date
+#'
+#' @param field  a single vector (the allowed classes are: numeric, integer, factor, logical, character, POSIXct/ POSIXlt for datetimes and Date for dates.
+#' @param cat_thres non negative integer value to set the number of unique values a numeric or integer field needs to have to be classified as continuous. (default is set to 30)
+#'
+#' @return a character string with either "cont", "cat", "datetime or "date"
+#' @export
+#'
+#' @author Emanuel Sommer
+classify_vector <- function(field, cat_thres = 30){
+  if(!is.numeric(cat_thres)){
+    stop("The cat_thres argument must be numeric!")
+  } else if(cat_thres < 0){
+    stop("The cat_thres argument must be non negative!")
+  }
+  act_class <- class(field)
+  num_unique <- length(unique(field))
+
+  if(("numeric" %in% act_class && num_unique > cat_thres) |
+      ("integer" %in% act_class && num_unique > cat_thres)){
+    return("cont")
+  } else if ("character" %in% act_class | "factor" %in% act_class   |
+              ("integer" %in% act_class && num_unique <= cat_thres) |
+              ("numeric" %in% act_class && num_unique <= cat_thres) |
+              "logical"%in% act_class){
+    return("cat")
+  } else if ("POSIXct" %in% act_class | "POSIXlt" %in% act_class){
+    return("datetime")
+  } else if ("Date" %in% act_class){
+    return("date")
+  } else {
+    stop(paste("Class of the input vector not specified:",act_class))
+  }
+}
+
