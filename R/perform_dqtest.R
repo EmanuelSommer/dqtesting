bccdd7e8a4acb97f0c1c1ccdbaa8eced18d62b5a
Emanuel Sommer <emanuel.sommer@web.de>
2020-02-18 00:07:01 +0100

    + summaries + checks + tests

--- R/perform_dqtest.R
+++ R/perform_dqtest.R
@@ -1,16 +1,21 @@
 
-
+# imports    tibble dyplr
 
 perform_dqtest <- function(x,
                            var_name = "", # vaiable name for nicer plots
                            cat_thres = 30, # int value
                            categories = NULL, # character vector
-                           range_min = NULL,
+                           range_min = NULL,# else Date minimum 0000-01-01, max 9999-12-31
                            range_max = NULL,
+                           tz = NULL, # can exchange the timezone else timezone of the input
                            exclude_values = NULL, #numeric value for continuous data # POISXct for datetime or Date for date #character for cat
                            exclude_smaller_than = NULL, #numeric value for continuous data # POISXct for datetime or Date for date
                            exclude_greater_than = NULL #numeric value for continuous data # POISXct for datetime or Date for date
                            ){
+  #check for valid input
+  if(!is.null(dim(x)) && !is.list(x)){stop("x must be a single vector")}
+  if(!is.character(var_name)){stop("If supplied the var_name argument must be character.")}
+  if(!(is.character(tz) | is.null(tz))){stop("If supplied the tz argument must be character.")}
   # check whether the vector x contains only missing values. In this case a further analysis is worthless.
   if(all(is.na(x))){
     return(list(pre_scheduled_return = "Yes, all NA."))
@@ -24,6 +29,7 @@ perform_dqtest <- function(x,
   rel_na <- abs_na/length_original
   # add information to output list
   output_list$classification <- classification
+  output_list$class_type <- class(x)
   output_list$initial_length <- length_original
   output_list$abs_na <- abs_na
   output_list$rel_na <- rel_na
@@ -53,8 +59,36 @@ perform_dqtest <- function(x,
       output_list$pre_scheduled_return <- "Yes, all not missing excluded."
       return(output_list)
     }
-
-
+    # summary statistics
+    summary_cont_list <- list(
+      mean = mean(x, na.rm = TRUE),
+      sd = sd(x, na.rm = TRUE),
+      quantiles11 = quantile(x, na.rm = TRUE, probs = seq(0,1,0.1), names = FALSE),
+      skew = e1071::skewness(x, na.rm = TRUE)
+    )
+    output_list$stat_summary <- summary_cont_list
+    # range check: (if ranges were supplied)
+    if(is.null(range_min) && is.null(range_max)){
+      output_list$range_check <- "No ranges were supplied."
+    } else {
+      if(!(is.null(range_min) | is.numeric(range_min))){stop("The ranges for continuous input must be numeric if given.")}
+      if(!(is.null(range_max) | is.numeric(range_max))){stop("The ranges for continuous input must be numeric if given.")}
+      # treat cases where only one range was supplied:
+      range_min <- dplyr::if_else(is.null(range_min), -Inf, range_min)
+      range_max <- dplyr::if_else(is.null(range_max), Inf, range_max)
+      act_min <- min(x,na.rm = T)
+      act_max <- max(x,na.rm = T)
+      if(act_min < range_min && act_max <= range_max){
+        range_check_num <- "Minimum out of range."
+      } else if (act_min >= range_min && act_max > range_max){
+        range_check_num <- "Maximum out of range."
+      } else if (act_min < range_min && act_max > range_max){
+        range_check_num <- "Maximum and minimum out of range."
+      } else {
+        range_check_num <- "In range."
+      }
+      output_list$range_check <- range_check_num
+    }
   } else if(classification == "cat"){ ######################################################################################
     #convert to a factor
     x_factor <- as.factor(x)
@@ -74,7 +108,23 @@ perform_dqtest <- function(x,
       output_list$pre_scheduled_return <- "Yes, all not missing excluded."
       return(output_list)
     }
-
+    # summary statistics
+    summary_tbl <- tibble(x = x_factor) %>%
+      mutate(x = as.character(x)) %>%
+      count(x) %>%
+      rename(absolute = n)%>%
+      mutate(relative = absolute/length_original)%>%
+      arrange(desc(absolute),x)
+    output_list$stat_summary <- summary_tbl
+    # categories check
+    if(is.null(categories)){
+      output_list$cat_check <- "No categories were specified."
+    } else {
+      if(!is.character(categories)){stop("categories must be supplied as a character vector.")}
+      output_list$cat_check <- dplyr::if_else(all(levels(x_factor) %in% categories),
+                                      "Only the required categories are present.",
+                                      "There are unspecified categories!")
+    }
   } else if(classification == "date"){ #####################################################################################
     # exclude values if requested
     if(!is.null(exclude_values)){
@@ -100,9 +150,40 @@ perform_dqtest <- function(x,
       output_list$pre_scheduled_return <- "Yes, all not missing excluded."
       return(output_list)
     }
+    # summary statistics
+    summary_date_list <- list(
+      mean = mean(x, na.rm = TRUE),
+      quantiles11 = lubridate::date(quantile(as.POSIXct(x), na.rm = TRUE, probs = seq(0,1,0.1), names = FALSE))
+    )
+    output_list$stat_summary <- summary_date_list
+    # range check: (if ranges were supplied)
+    if(is.null(range_min) && is.null(range_max)){
+      output_list$range_check <- "No ranges were supplied."
+    } else {
+      if(!(is.null(range_min) | lubridate::is.Date(range_min))){stop("The ranges for Date input must be Dates if given.")}
+      if(!(is.null(range_max) | lubridate::is.Date(range_max))){stop("The ranges for Date input must be Dates if given.")}
+      # treat cases where only one range was supplied:
+      range_min <- dplyr::if_else(is.null(range_min), as.Date("0000-01-01"), range_min)
+      range_max <- dplyr::if_else(is.null(range_max), as.Date("9999-12-31"), range_max)
+      act_min <- min(x,na.rm = T)
+      act_max <- max(x,na.rm = T)
+      if(act_min < range_min && act_max <= range_max){
+        range_check_date <- "Minimum out of range."
+      } else if (act_min >= range_min && act_max > range_max){
+        range_check_date <- "Maximum out of range."
+      } else if (act_min < range_min && act_max > range_max){
+        range_check_date <- "Maximum and minimum out of range."
+      } else {
+        range_check_date <- "In range."
+      }
+      output_list$range_check <- range_check_date
+    }
   } else if(classification == "datetime"){ #################################################################################
-    # convert into POSIXct (from potentially POSIXlt)
-    x <- as.POSIXct(x)
+    # convert into POSIXct (from potentially POSIXlt) and adjust timezone
+    if(is.null(tz)){
+      tz <- lubridate::tz(x)
+    }
+    x <- as.POSIXct(x, tz = tz)
     # exclude values if requested
     if(!is.null(exclude_values)){
       if(!lubridate::is.POSIXct(exclude_values)){stop("For datetime input the values to be excluded must be given as POSIXct.")}
@@ -126,6 +207,34 @@ perform_dqtest <- function(x,
     if(length(x[!is.na(x)]) == 0){
       output_list$pre_scheduled_return <- "Yes, all not missing excluded."
       return(output_list)
+    }
+    # summary statistics
+    summary_datetime_list <- list(
+      mean = mean(x, na.rm = TRUE),
+      quantiles11 = quantile(x, na.rm = TRUE, probs = seq(0,1,0.1), names = FALSE)
+    )
+    output_list$stat_summary <- summary_datetime_list
+    # range check: (if ranges were supplied)
+    if(is.null(range_min) && is.null(range_max)){
+      output_list$range_check <- "No ranges were supplied."
+    } else {
+      if(!(is.null(range_min) | lubridate::is.POSIXct(range_min))){stop("The ranges for datetime input must be POSIXct if given.")}
+      if(!(is.null(range_max) | lubridate::is.POSIXct(range_max))){stop("The ranges for datetime input must be POSIXct if given.")}
+      # treat cases where only one range was supplied:
+      range_min <- dplyr::if_else(is.null(range_min), as.POSIXct("0000-01-01 00:00:00",tz = tz), range_min)
+      range_max <- dplyr::if_else(is.null(range_max), as.POSIXct("9999-12-31 23:59:59",tz = tz), range_max)
+      act_min <- min(x,na.rm = T)
+      act_max <- max(x,na.rm = T)
+      if(act_min < range_min && act_max <= range_max){
+        range_check_datetime <- "Minimum out of range."
+      } else if (act_min >= range_min && act_max > range_max){
+        range_check_datetime <- "Maximum out of range."
+      } else if (act_min < range_min && act_max > range_max){
+        range_check_datetime <- "Maximum and minimum out of range."
+      } else {
+        range_check_datetime <- "In range."
+      }
+      output_list$range_check <- range_check_datetime
     }
   } else{
     stop(paste("Class of the input vector not specified!"))
--- tests/testthat/test-perform_dqtest.R
+++ tests/testthat/test-perform_dqtest.R
@@ -42,6 +42,19 @@ test_that("NA counts/ relative", {
   expect_equal(perform_dqtest(x = c("abc","def","xyz",NA))$rel_na , 0.25)
 })
 
+test_that("Class type", {
+  expect_equal(perform_dqtest(x = c(1:10,NA))$class_type , "integer")
+  expect_equal(perform_dqtest(x = c(1:10,NA,0.1))$class_type , "numeric")
+  expect_equal(perform_dqtest(x = c("abc",NA))$class_type , "character")
+  expect_equal(perform_dqtest(x = as.POSIXct(c("2000-12-12 00:12:33",
+                                               "3000-01-01 11:11:23",
+                                               "0110-02-22 00:00:01",
+                                               NA)))$class_type , c("POSIXct", "POSIXt"))
+  expect_equal(perform_dqtest(x = as.Date(c("2000-12-12",
+                                               "3000-01-01",
+                                               "0110-02-22",
+                                               NA)))$class_type , "Date")
+})
 
 ##############################################################################################################
 #
@@ -87,7 +100,6 @@ test_that("Exclusions continuous", {
                               exclude_greater_than = 30)$pre_scheduled_return , "Yes, all not missing excluded.")
 })
 
-
 test_that("Exclusions categorical", {
   expect_equal(perform_dqtest(x = c("abc",rep("def",4),NA,"xyz",rep("test",3)))$exclusions , "No exclusions.")
   expect_equal(perform_dqtest(x = c("abc",rep("def",4),NA,"xyz",rep("test",3)),
@@ -111,7 +123,6 @@ test_that("Exclusions categorical", {
   expect_equal(perform_dqtest(x = c("abc",rep("def",4),NA,"xyz",rep("test",3)),
                               exclude_values = c("abc","test","def","xyz"))$pre_scheduled_return , "Yes, all not missing excluded.")
 })
-
 
 test_that("Exclusions date", {
   expect_equal(perform_dqtest(x = as.Date(c("2000-12-12",
@@ -190,7 +201,6 @@ test_that("Exclusions date", {
                               exclude_greater_than = as.Date("0001-12-12"))$pre_scheduled_return , "Yes, all not missing excluded.")
 })
 
-
 test_that("Exclusions datetime", {
   expect_equal(perform_dqtest(x = as.POSIXct(c("2000-12-12 00:12:33",
                                                rep("3000-01-01 11:11:23",7),
@@ -268,6 +278,126 @@ test_that("Exclusions datetime", {
                               exclude_greater_than = as.POSIXct("0001-12-12"))$pre_scheduled_return , "Yes, all not missing excluded.")
 })
 
+##############################################################################################################
+#
+#                        Summary tests
+#
+##############################################################################################################
 
+test_that("Summary continuous", {
+  expect_equal(perform_dqtest(x = c(1:100,NA))$stat_summary$mean , 50.5)
+  expect_equal(perform_dqtest(x = c(1:100,NA,0.1))$stat_summary$mean , 50.00099)
+  expect_equal(round(perform_dqtest(x = c(1:100,NA,0.1))$stat_summary$sd,5) , 29.29847)
+  expect_equal(perform_dqtest(x = c(1:100,NA,0.1))$stat_summary$quantiles11 , c(0.1,10.0,20.0,30.0,40.0,50.0,60.0,70.0,80.0,90.0,100.0))
+  expect_equal(perform_dqtest(x = c(1:100,NA,0.1))$stat_summary$skew , 0.0001942934)
+})
 
+test_that("Summary categorical", {
+  expect_equal(perform_dqtest(x = c(NA,rep("abc",4),rep("def",3),"xyz","test"))$stat_summary$x , c("abc","def","test","xyz",NA))
+  expect_equal(perform_dqtest(x = c(NA,rep("abc",4),rep("def",3),"xyz","test"))$stat_summary$absolute , c(4,3,1,1,1))
+  expect_equal(perform_dqtest(x = c(NA,rep("abc",4),rep("def",3),"xyz","test"))$stat_summary$relative , c(0.4,0.3,0.1,0.1,0.1))
+})
+
+test_that("Summary date", {
+  expect_equal(lubridate::day(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")))$stat_summary$mean),
+               lubridate::day(as.Date("2001-10-23")))
+  expect_equal(lubridate::month(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")))$stat_summary$mean),
+               lubridate::month(as.Date("2001-10-23")))
+  expect_equal(lubridate::year(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")))$stat_summary$mean),
+               lubridate::year(as.Date("2001-10-23")))
+  expect_equal(lubridate::day(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")))$stat_summary$quantiles11[6]),
+               lubridate::day(as.Date("2001-02-01")))
+  expect_equal(lubridate::month(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")))$stat_summary$quantiles11[6]),
+               lubridate::month(as.Date("2001-02-01")))
+  expect_equal(lubridate::year(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")))$stat_summary$quantiles11[6]),
+               lubridate::year(as.Date("2001-02-01")))
+})
+
+test_that("Summary datetime", {
+  expect_equal(perform_dqtest(x = as.POSIXct(c("2000-01-01 00:11:32",rep("2001-02-01 11:02:00",10),"2010-11-11")))$stat_summary$mean , as.POSIXct("2001-10-23 11:00:00"))
+  expect_equal(perform_dqtest(x = as.POSIXct(c("2000-01-01 00:11:32",rep("2001-02-01 11:02:00",10),"2010-11-11")))$stat_summary$quantiles11 , as.POSIXct(c("2000-01-01 CET", "2001-02-01 CET",
+                                                                                                                                                         "2001-02-01 CET", "2001-02-01 CET",
+                                                                                                                                                         "2001-02-01 CET", "2001-02-01 CET",
+                                                                                                                                                         "2001-02-01 CET", "2001-02-01 CET",
+                                                                                                                                                         "2001-02-01 CET", "2001-02-01 CET",
+                                                                                                                                                         "2010-11-11 CET")))
+})
+
+##############################################################################################################
+#
+#                        Checks tests
+#
+##############################################################################################################
+
+test_that("Range check continuous", {
+  expect_equal(perform_dqtest(x = c(1:100,NA))$range_check , "No ranges were supplied.")
+  expect_equal(perform_dqtest(x = c(1:100,NA), range_min = 1, range_max = 100)$range_check,
+               "In range.")
+  expect_equal(perform_dqtest(x = c(1:100,NA), range_min = 3, range_max = 100)$range_check,
+               "Minimum out of range.")
+  expect_equal(perform_dqtest(x = c(1:100,NA), range_min = -111, range_max = 99)$range_check,
+               "Maximum out of range.")
+  expect_equal(perform_dqtest(x = c(1:100,NA), range_min = 11, range_max = 99)$range_check,
+               "Maximum and minimum out of range.")
+  expect_error(perform_dqtest(x = c(1:100,NA), range_min = "11", range_max = 99)$range_check)
+})
+
+test_that("Categories check", {
+  expect_equal(perform_dqtest(x = c(NA,rep("abc",4),rep("def",3),"xyz","test"))$cat_check ,
+               "No categories were specified.")
+  expect_equal(perform_dqtest(x = c(NA,rep("abc",4),rep("def",3),"xyz","test"),
+                              categories = c("gg"))$cat_check ,
+               "There are unspecified categories!")
+  expect_equal(perform_dqtest(x = c(NA,rep("abc",4),rep("def",3),"xyz","test"),
+                              categories = c("abc","defg"))$cat_check ,
+               "There are unspecified categories!")
+  expect_equal(perform_dqtest(x = c(NA,rep("abc",4),rep("def",3),"xyz","test"),
+                              categories = c("abc","def","test","xyz"))$cat_check ,
+               "Only the required categories are present.")
+  expect_equal(perform_dqtest(x = c(NA,rep("abc",4),rep("def",3),"xyz","test"),
+                              categories = c("abc","def","test","xyz","ghi"))$cat_check ,
+               "Only the required categories are present.")
+  expect_error(perform_dqtest(x = c(NA,rep("abc",4),rep("def",3),"xyz","test"),
+                              categories = 22)$cat_check)
+})
+
+test_that("Range check date", {
+  expect_equal(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")))$range_check , "No ranges were supplied.")
+  expect_equal(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")),
+                              range_min = as.Date("2000-01-01"), range_max = as.Date("2010-11-11"))$range_check,
+               "In range.")
+  expect_equal(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")),
+                              range_min = as.Date("2000-01-02"), range_max = as.Date("2010-11-11"))$range_check,
+               "Minimum out of range.")
+  expect_equal(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")),
+                              range_min = as.Date("2000-01-01"), range_max = as.Date("2010-11-10"))$range_check,
+               "Maximum out of range.")
+  expect_equal(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")),
+                              range_min = as.Date("2000-02-01"), range_max = as.Date("2010-10-10"))$range_check,
+               "Maximum and minimum out of range.")
+  expect_error(perform_dqtest(x = as.Date(c("2000-01-01",rep("2001-02-01",10),"2010-11-11")),
+                              range_min = "2000-01-01", range_max = as.Date("2010-11-10"))$range_check)
+})
+
+test_that("Range check datetime", {
+  expect_equal(perform_dqtest(x = as.POSIXct(c("2000-01-01 00:11:32",rep("2001-02-01 11:02:00",10),"2010-11-11"), tz = "UTC"))$range_check ,
+               "No ranges were supplied.")
+  expect_equal(perform_dqtest(x = as.POSIXct(c("2000-01-01 00:11:32",rep("2001-02-01 11:02:00",10),"2010-11-11"), tz = "UTC"),
+                              range_min = as.POSIXct("1999-12-31 00:11:01", tz = "UTC"), range_max = as.POSIXct("2010-11-11 11:02:00", tz = "UTC"))$range_check,
+               "In range.")
+  expect_equal(perform_dqtest(x = as.POSIXct(c("2000-01-01 23:11:32",rep("2001-02-01 11:02:00",10),"2010-11-11 00:00:00"), tz = "UTC"),
+                              range_min = as.POSIXct("2000-01-01 00:00:01", tz = "UTC"), range_max = as.POSIXct("2010-11-11 11:02:00", tz = "UTC"))$range_check,
+               "In range.")
+  expect_equal(perform_dqtest(x = as.POSIXct(c("2000-01-01 00:11:32",rep("2001-02-01 11:02:00",10),"2010-11-11"), tz = "UTC"),
+                              range_min = as.POSIXct("2000-01-02", tz = "UTC"), range_max = as.POSIXct("2010-11-12", tz = "UTC"))$range_check,
+               "Minimum out of range.")
+  expect_equal(perform_dqtest(x = as.POSIXct(c("2000-01-01 00:11:32",rep("2001-02-01 11:02:00",10),"2010-11-11"), tz = "UTC"),
+                              range_min = as.POSIXct("2000-01-01", tz = "UTC"), range_max = as.POSIXct("2010-11-10", tz = "UTC"))$range_check,
+               "Maximum out of range.")
+  expect_equal(perform_dqtest(x = as.POSIXct(c("2000-01-01 00:11:32",rep("2001-02-01 11:02:00",10),"2010-11-11"), tz = "UTC"),
+                              range_min = as.POSIXct("2000-02-02", tz = "UTC"), range_max = as.POSIXct("2010-10-10", tz = "UTC"))$range_check,
+               "Maximum and minimum out of range.")
+  expect_error(perform_dqtest(x = as.POSIXct(c("2000-01-01 00:11:32",rep("2001-02-01 11:02:00",10),"2010-11-11"), tz = "UTC"),
+                              range_min = "2000-01-01", range_max = as.Date("2010-11-10"))$range_check)
+})
 
